7185702c13b7023ed097f41897401c0eb64f7470
Jesus Erwin Suarez <mrjesuserwinsuarez@gmail.com>
2016-04-05 11:01:25 +0800

    theme update

--- public_html/trustees.smsfassociation.com/wp-content/themes/Avada/enrolonly.php
+++ public_html/trustees.smsfassociation.com/wp-content/themes/Avada/enrolonly.php
@@ -1,24 +1,24 @@
-<?php
-if($_POST){
-include_once('../wp-config.php');
-
-
-$email = $_POST['email'];
-$course_id = $_POST['course_id'];
-$user = get_user_by( 'email', $email );
-$user_id = $user->ID;
-
-
-//echo print_r($user, true);
-$enrol = ld_update_course_access($user_id, $course_id, $remove = false);
-//echo print_r($enrol, true);
-//header("Location: ".$_POST['link_url']);
-//die();
-}
-//else {
-//header("Location: https://app.thesmsfacademy.com.au");
-//die();
-//}
-
-
+<?php
+if($_POST){
+include_once('../wp-config.php');
+
+
+$email = $_POST['email'];
+$course_id = $_POST['course_id'];
+$user = get_user_by( 'email', $email );
+$user_id = $user->ID;
+
+
+//echo print_r($user, true);
+$enrol = ld_update_course_access($user_id, $course_id, $remove = false);
+//echo print_r($enrol, true);
+//header("Location: ".$_POST['link_url']);
+//die();
+}
+//else {
+//header("Location: https://app.thesmsfacademy.com.au");
+//die();
+//}
+
+
 ?>
